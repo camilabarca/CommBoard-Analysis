ee746a849cd8bc70855657c7be3a31953b231fc6
Camila <camila.labarca@ug.uchile.cl>
2023-05-10 11:29:05 -0400

    sort by table title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,8 @@
   	import firebase from '../lib/firebase/firebase';
 
 	let data = [];
+	let sortColumn = "date";
+    let sortAsc = true;
 
 	onMount(async () => {
 		const snapshot = await firebase.database().ref('interactions').once('value');
@@ -14,8 +16,49 @@
 			data[item].date = date
 			data[item].time = time
 		}
-		console.log(data)
+		sortTable();
 	});
+
+	function downloadCsv() {
+        const rows = data
+            .map(
+                (data) =>
+                    `${data.date},${data.time},${data.guardian},${data.subject},${data.sound}`
+            )
+            .join("\n");
+        const csv = `Date,Time,Guardian,Subject,Sound\n${rows}`;
+        const blob = new Blob([csv], { type: "text/csv" });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = "interactions.csv";
+        link.click();
+    }
+
+	function sortTable() {
+        data = data.sort((a, b) => {
+            const aValue = a[sortColumn];
+            const bValue = b[sortColumn];
+
+            if (aValue < bValue) {
+                return sortAsc ? -1 : 1;
+            } else if (aValue > bValue) {
+                return sortAsc ? 1 : -1;
+            } else {
+                return 0;
+            }
+        });
+    }
+
+	function handleTitleClick(column){
+		if (column === sortColumn){
+			sortAsc = !sortAsc;
+		} else {
+			sortColumn = column;
+			sortAsc = true;
+		}
+		sortTable();
+	}
 </script>
 
 <svelte:head>
@@ -24,16 +67,14 @@
 </svelte:head>
 
 <section>
-
-	<button>Download</button>
 	<table>
 		<thead>
 			<tr>
-				<th>Date</th>
-				<th>Time</th>
-				<th>Guardian</th>
-				<th>Subject</th>
-				<th>Sound</th>
+				<th on:click={() => handleTitleClick("date")}>Date {sortColumn === "date" ? (sortAsc ? "▲" : "▼") : ""}</th>
+				<th on:click={() => handleTitleClick("time")}>Time {sortColumn === "time" ? (sortAsc ? "▲" : "▼") : ""}</th>
+				<th on:click={() => handleTitleClick("guardian")}>Guardian {sortColumn === "guardian" ? (sortAsc ? "▲" : "▼") : ""}</th>
+				<th on:click={() => handleTitleClick("subject")}>Subject {sortColumn === "subject" ? (sortAsc ? "▲" : "▼") : ""}</th>
+				<th on:click={() => handleTitleClick("sound")}>Sound {sortColumn === "sound" ? (sortAsc ? "▲" : "▼") : ""}</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -49,6 +90,7 @@
 			{/each}
 		  </tbody>
 	</table>
+	<button on:click={downloadCsv}>Download</button>
 
 </section>
 
@@ -61,28 +103,41 @@
 		flex: 0.6;
 	}
 	table {
-	width: 100%;
-	border-collapse: collapse;
+		width: 100%;
+		border-collapse: collapse;
 	}
 
 	th, td {
-	padding: 12px;
-	text-align: left;
-	border-bottom: 1px solid #ddd;
+		padding: 12px;
+		text-align: left;
+		border-bottom: 1px solid #ddd;
 	}
 
 	th {
-	background-color: #f2f2f2;
-	color: #333;
-	font-weight: bold;
+		background-color: #f2f2f2;
+		color: #333;
+		font-weight: bold;
+		cursor: pointer;
 	}
 
 	tr:nth-child(even) {
-	background-color: #f9f9f9;
+		background-color: #f9f9f9;
 	}
 
 	tr:hover {
-	background-color: #e9e9e9;
+		background-color: #e9e9e9;
+	}
+	button {
+		padding: 10px 20px;
+		background-color: #4a90e2;
+		color: #fff;
+		border: none;
+		border-radius: 5px;
+		font-size: 16px;
+		position: absolute;
+		bottom: 10px;
+		left: 10px;
+		cursor: pointer;
 	}
 
 
