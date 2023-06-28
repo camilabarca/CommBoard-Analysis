83ea18fdf331acfab46b6a6928936cb4b1fb3379
Camila <camila.labarca@ug.uchile.cl>
2023-06-28 15:56:54 -0400

    contact info

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -15,10 +15,32 @@
 	  font-weight: bold;
 	  margin-right: 16px;
 	}
+	.contact-info {
+        font-size: 10px;
+        line-height: 0;
+
+		padding-left: 10%;
+    }
+	.contact {
+		display: flex;
+		padding-left: 50%;
+	}
   </style>
   
   <div class="header">
 	<h1 class="title">{title}</h1>
+	<div class="contact">
+		<div class="contact-info">
+			<p>Camila Labarca</p>
+			<a href="https://github.com/camilabarca/CommBoard-Front">Github repository</a>
+			<p>camila.labarca@ug.uchile.cl</p>
+		</div>
+		<div class="contact-info">
+			<p>What did you think of the page? Answer our form:</p>
+			<a href="https://docs.google.com/forms/d/e/1FAIpQLSdLVoKteYsTsFjBm_BETwsK-yXClOTe5_jTh6Nf1wKVqiTSvA/viewform?usp=sf_link">Click here!</a>
+		</div>
+	</div>
+	
   </div>
   
   
